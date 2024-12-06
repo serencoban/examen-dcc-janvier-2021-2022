6b9fe685b19de79b088c94ad1df895f6450ac62b
seren <seren.coban@student.hepl.be>
2024-12-06 11:03:05 +0100

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
-{import(fonts)}
+import {fonts} from "./fonts"
+import {settings as s} from "./settings"
+
+const ulCardElement = document.querySelector(s.ulCardSelector);   // s = settings
+
 //parcourire les police des caracteres, le tableau dcp avec une boucle
 for (const font of fonts) {
-    console.log(font)
+    ulCardElement.insertAdjacentHTML('beforeend', `
+    <li data-font-name="${font.name}" data-family="famille" class=\\'app__item\\'>
+     <div class="app__item__info"><span class="app__item__info__name">nom</span>
+     <span class="app__item__info__info">famille - auteur</span>
+     </div>
+     <img class=\\'app__item__font\\' src=\\'./assets/fonts/file.svg\\' alt=\\'Aa, abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ\\'>
+     '</li>`)
 }
